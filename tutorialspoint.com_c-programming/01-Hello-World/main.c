a82882fc13ea81239988c834a9f666832eded259
General Jack O'Neill <68293825+StarGate-One@users.noreply.github.com>
2022-01-29 23:57:49 -0500

    Add new C lessons from tutorialspoint.com

---
+++ tutorialspoint.com_c-programming/01-Hello-World/main.c
@@ -0,0 +1,17 @@
+/*
+ * File:    main.cpp
+ * Written: 01-29-2022
+ * Author:  StarGate-One
+ * Program: Hello World
+ * Project: 01-Hello-World
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    /* my first program in C */
+    printf("Hello, World!\n");
+
+    return 0;
+}
